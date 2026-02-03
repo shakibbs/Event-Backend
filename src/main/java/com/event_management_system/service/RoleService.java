5379283659be3638175afac5c2b5e4c8f0b4577d
Shakib <rakibulislamshakib570@gmail.com>
2026-02-03 18:53:10 +0600

    Add DependsOn to ensure permissions are created before roles

--- src/main/java/com/event_management_system/service/RoleService.java
+++ src/main/java/com/event_management_system/service/RoleService.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,6 +25,7 @@ import com.event_management_system.repository.RoleRepository;
 import jakarta.annotation.PostConstruct;
 
 @Service
+@DependsOn("permissionService")
 public class RoleService {
 
     @Autowired
